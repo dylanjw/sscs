3ba1c90358e6167f8599994e14a4d526c8e6a141
Dylan Wilson <dylanjw@protonmail.com>
2021-07-11 09:51:39 -0700

    Make less hacky heroku settings

--- sscs/settings/base.py
+++ sscs/settings/base.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -150,3 +151,4 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 STATICFILES_DIRS = [
     BASE_DIR / "static/js",
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
