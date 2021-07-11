ba2c7ee7e0d49b32f33ae423520b7e357fdd826b
Dylan Wilson <dylanjw@protonmail.com>
2021-07-11 11:15:43 -0700

    Fix bad module import

--- sscs/asgi.py
+++ sscs/asgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-import channels.asgi
+import channels.layers
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sscs.settings.base')
 
 application = get_asgi_application()
-channel_layer = channels.asgi.get_channel_layer()
+channel_layer = channels.layers.get_channel_layer()
