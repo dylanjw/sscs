1908447c63b0821b367e104b42a8a6faf22f13b3
Dylan Wilson <dylanjw@protonmail.com>
2021-07-11 09:09:43 -0700

    Add STATIC_ROOT for heroku

--- sscs/sscs/settings.py
+++ sscs/sscs/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
@@ -147,5 +148,5 @@ INTERNAL_IPS = [
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static/js",
+    BASE_DIR / "sscs/static/js",
 ]
