9ccae103e33e6cfa27c128ddd18ed3789dc238ca
Dylan Wilson <dylanjw@protonmail.com>
2021-07-11 11:29:37 -0700

    Fix syntax error

--- sscs/asgi.py
+++ sscs/asgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 import django
-import channels.routing import get_default_application()
+from channels.routing import get_default_application()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sscs.settings.base')
 django.setup()
